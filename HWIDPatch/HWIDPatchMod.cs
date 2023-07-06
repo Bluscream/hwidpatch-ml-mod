b5b6496accccc46e10d9c4c8c6344d6323ed7b8b
Bluscream <Bluscream@users.noreply.github.com>
2023-07-06 10:07:03 +0200

    a

--- HWIDPatch/HWIDPatchMod.cs
+++ HWIDPatch/HWIDPatchMod.cs
@@ -1,63 +1,66 @@
 using System;
 using System.Linq;
-using System.Reflection;
 using HWIDPatch;
 using MelonLoader;
-using UnhollowerBaseLib;
 using UnityEngine;
+using Harmony;
 
-[assembly:MelonInfo(typeof(HwidPatchMod), "HWIDPatch", "1.0.2", "knah", "https://github.com/knah/ML-UniversalMods")]
+[assembly:MelonInfo(typeof(HwidPatchMod), "HWIDPatch (Mono)", "2.0.0", "Bluscream, knah", "https://github.com/Bluscream/hwidpatch-ml-mod")]
 [assembly:MelonGame]
 
 namespace HWIDPatch
 {
     public class HwidPatchMod : MelonMod
     {
-        private static Il2CppSystem.Object ourGeneratedHwidString;
-        
-        public override unsafe void OnApplicationStart()
+        internal static MelonPreferences_Entry<string> spoofedHWID;
+
+        internal static void Log(object message, LogType type = LogType.Log) {
+            //if (!Preferences.EnableLogging.Value) return;
+            var msg = message.ToString();
+            MelonLogger.Msg(msg);
+        }
+
+        public override void OnInitializeMelon()
         {
             try
             {
                 var category = MelonPreferences.CreateCategory("HWIDPatch", "HWID Patch");
-                var hwidEntry = category.CreateEntry("HWID", "", is_hidden: true);
+                spoofedHWID = category.CreateEntry("HWID", "", is_hidden: true);
 
-                var newId = hwidEntry.Value;
+                var newId = spoofedHWID.Value;
                 if (newId.Length != SystemInfo.deviceUniqueIdentifier.Length)
                 {
                     var random = new System.Random(Environment.TickCount);
                     var bytes = new byte[SystemInfo.deviceUniqueIdentifier.Length / 2];
                     random.NextBytes(bytes);
                     newId = string.Join("", bytes.Select(it => it.ToString("x2")));
-                    MelonLogger.Msg("Generated and saved a new HWID");
-                    hwidEntry.Value = newId;
+                    Log("Generated and saved a new HWID");
+                    spoofedHWID.Value = newId;
                     category.SaveToFile(false);
                 }
 
-                ourGeneratedHwidString = new Il2CppSystem.Object(IL2CPP.ManagedStringToIl2Cpp(newId));
-
-                var icallName = "UnityEngine.SystemInfo::GetDeviceUniqueIdentifier";
-                var icallAddress = IL2CPP.il2cpp_resolve_icall(icallName);
-                if (icallAddress == IntPtr.Zero)
-                {
-                    MelonLogger.Error("Can't resolve the icall, not patching");
-                    return;
-                }
-                
-                MelonUtils.NativeHookAttach((IntPtr) (&icallAddress),
-                    typeof(HwidPatchMod).GetMethod(nameof(GetDeviceIdPatch),
-                        BindingFlags.Static | BindingFlags.NonPublic)!.MethodHandle.GetFunctionPointer());
-
-                MelonLogger.Msg("Patched HWID; below two should match:");
-                MelonLogger.Msg($"Current: {SystemInfo.deviceUniqueIdentifier}");
-                MelonLogger.Msg($"Target:  {newId}");
+                Log("Patched HWID; below two should match:");
+                Log($"Current: {SystemInfo.deviceUniqueIdentifier}");
+                Log($"Target:  {newId}");
             }
             catch (Exception ex)
             {
                 MelonLogger.Error(ex.ToString());
             }
         }
-
-        private static IntPtr GetDeviceIdPatch() => ourGeneratedHwidString.Pointer;
+    }
+    [HarmonyPatch(typeof(SystemInfo))]
+    [HarmonyPatch(nameof(SystemInfo.deviceUniqueIdentifier), PropertyMethod.Getter)]
+    [Obsolete]
+    static class Patch {
+        static string Postfix(string __result) {
+            HwidPatchMod.Log($"deviceUniqueIdentifier getter called: {__result}");
+            var newId = HwidPatchMod.spoofedHWID.Value;
+            if (!string.IsNullOrEmpty(newId)) {
+                HwidPatchMod.Log($"Presenting spoofed HWID: {newId}");
+                return newId;
+            }
+            return __result;
+        }
     }
 }
